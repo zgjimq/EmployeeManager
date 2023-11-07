4ed8925192b1448eae3b4befddd9fc4fb7dde0f3
ZgjimQ <zq56042@ubt-uni.net>
2023-11-07 23:05:58 +0100

    Mesazhi

--- src/main/java/org/student/com/entity/Department.java
+++ src/main/java/org/student/com/entity/Department.java
@@ -1,5 +1,31 @@
 package org.student.com.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "Department_Name")
 	private String name;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
 }
