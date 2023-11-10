2afb67c093861f7bc28970db6d6a8f070d3a5e07
ZgjimQ <zq56042@ubt-uni.net>
2023-11-10 15:49:09 +0100

    Updates

--- src/main/java/org/student/com/entity/Department.java
+++ src/main/java/org/student/com/entity/Department.java
@@ -1,30 +1,56 @@
 package org.student.com.entity;
 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "Department")
 public class Department {
+	//departmentNumber, departmentOwnersName, departmentOwnersPhoneNr
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="Department_Id")
 	private Long id;
 	@Column(name = "Department_Name")
-	private String name;
+	private String departmentName;
+	@Column(name = "Department_Phone_Number")
+	private String departmentPhoneNr;
+	
+	public Department() {
+		super();
+	}
+	public Department(Long id, String departmentName, String departmentPhoneNr) {
+		super();
+		this.id = id;
+		this.departmentName = departmentName;
+		this.departmentPhoneNr = departmentPhoneNr;
+	}
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getName() {
-		return name;
+	public String getDepartmentName() {
+		return departmentName;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
 	}
+	public String getDepartmentPhoneNr() {
+		return departmentPhoneNr;
+	}
+	public void setDepartmentPhoneNr(String departmentPhoneNr) {
+		this.departmentPhoneNr = departmentPhoneNr;
+	}
+	
+	
+	
 	
 	
 	
