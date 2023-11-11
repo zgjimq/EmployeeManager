986ac8aef15ec84411450c621aa6063b8534facc
ZgjimQ <zq56042@ubt-uni.net>
2023-11-11 13:33:34 +0100

    Added the @Restcontroller and @RequestMapping

--- src/main/java/org/student/com/controller/DepartmentController.java
+++ src/main/java/org/student/com/controller/DepartmentController.java
@@ -1,6 +1,11 @@
 package org.student.com.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.student.com.repository.DepartmentRepository;
 
+
+@RestController
+@RequestMapping("/api/departments")
 public class DepartmentController {
 	private DepartmentRepository departmentRepository;
 	
