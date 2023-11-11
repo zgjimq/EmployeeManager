03c05a9041c00064c444a885ed6baa4a2d1a6426
ZgjimQ <zq56042@ubt-uni.net>
2023-11-11 13:43:33 +0100

    Added the @Post method for creating an Department

--- src/main/java/org/student/com/controller/DepartmentController.java
+++ src/main/java/org/student/com/controller/DepartmentController.java
@@ -1,6 +1,10 @@
 package org.student.com.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.student.com.entity.Department;
 import org.student.com.repository.DepartmentRepository;
 
 
@@ -13,5 +17,9 @@ public class DepartmentController {
 		this.departmentRepository=departmentRepository;
 	}
 	
+	@PostMapping(path = "create")
+	public ResponseEntity<Department>createDepartment(@RequestBody Department department){
+		return ResponseEntity.ok(departmentRepository.save(department));
+	}
 	
 }
