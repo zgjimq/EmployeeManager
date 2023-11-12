8fb24093adc732f95918630adb3df89ec78e0c0d
ZgjimQ <zq56042@ubt-uni.net>
2023-11-12 13:03:41 +0100

    Methon getAll in DepartmentController

--- src/main/java/org/student/com/controller/DepartmentController.java
+++ src/main/java/org/student/com/controller/DepartmentController.java
@@ -1,5 +1,8 @@
 package org.student.com.controller;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,8 @@ public class DepartmentController {
 	public ResponseEntity<Department>createDepartment(@RequestBody Department department){
 		return ResponseEntity.ok(departmentRepository.save(department));
 	}
-	
+	@GetMapping(path = "get")
+	public List<Department>getDepartments(){
+		return departmentRepository.findAll();
+	}
 }
