763a7194699ff2cb105483351cbe15a7165918f4
ZgjimQ <zq56042@ubt-uni.net>
2023-11-12 13:07:55 +0100

    Added th crossorigine annotation

--- src/main/java/org/student/com/controller/DepartmentController.java
+++ src/main/java/org/student/com/controller/DepartmentController.java
@@ -2,6 +2,7 @@ package org.student.com.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.student.com.repository.DepartmentRepository;
 
 @RestController
 @RequestMapping("/api/departments")
+@CrossOrigin
 public class DepartmentController {
 	private DepartmentRepository departmentRepository;
 	
