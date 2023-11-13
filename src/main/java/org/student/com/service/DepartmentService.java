a170e7394932205b90d5ea0c00517d4ac3eb2012
ZgjimQ <zq56042@ubt-uni.net>
2023-11-13 11:27:49 +0100

    New method for service class to create or update an user

--- src/main/java/org/student/com/service/DepartmentService.java
+++ src/main/java/org/student/com/service/DepartmentService.java
@@ -1,5 +1,24 @@
 package org.student.com.service;
 
+
+import org.springframework.stereotype.Service;
+
+import org.student.com.entity.Department;
+import org.student.com.repository.DepartmentRepository;
+
+@Service
 public class DepartmentService {
+	
+	private DepartmentRepository departmentRepository;
+	
+	public DepartmentService(DepartmentRepository departmentRepository) {
+		this.departmentRepository=departmentRepository;
+	}
+	
+	
+	public Department createOrUpdateDepartment(Department department){
+		return departmentRepository.save(department);
+	}
+	
 
 }
