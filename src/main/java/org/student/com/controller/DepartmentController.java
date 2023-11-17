e3c7026888cd917447ede55565f393bd966865af
ZgjimQ <zq56042@ubt-uni.net>
2023-11-17 19:57:21 +0100

    Updated the controller methodes that works with service

--- src/main/java/org/student/com/controller/DepartmentController.java
+++ src/main/java/org/student/com/controller/DepartmentController.java
@@ -1,10 +1,16 @@
 package org.student.com.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.student.com.entity.Department;
 import org.student.com.service.DepartmentService;
 
 
@@ -27,10 +33,26 @@ public class DepartmentController {
 	public List<Department>getDepartments(){
 		return departmentRepository.findAll();
 	}*/
-	private DepartmentService departmentService;
+	private final DepartmentService departmentService;
 	
 	public DepartmentController(DepartmentService departmentService) {
 		this.departmentService=departmentService;
 	}
 	
+	@PostMapping(path = "/createOrUpdate")
+	public Department createDepartment(@RequestBody Department department) {
+		return departmentService.createOrUpdateDepartment(department);
+	}
+	@GetMapping(path = "/getAll")
+	public List<Department>getAllDepartments(){
+		return departmentService.getDepartments();
+	}
+	@DeleteMapping(path = "/deleteById")
+	public ResponseEntity<?>deleteDepartment(Long id){
+		return departmentService.deleteById(id);
+	}
+	@GetMapping(path = "/getById")
+	public Department getDepartmentById(Long id) {
+		return departmentService.getDepartmentById(id);
+	}
 }
