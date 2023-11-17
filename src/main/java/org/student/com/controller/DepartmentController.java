de701c046466485c52fab0949711974bc40c145e
ZgjimQ <zq56042@ubt-uni.net>
2023-11-17 20:16:01 +0100

    Added the @PathVariable for methods

--- src/main/java/org/student/com/controller/DepartmentController.java
+++ src/main/java/org/student/com/controller/DepartmentController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,11 +49,11 @@ public class DepartmentController {
 		return departmentService.getDepartments();
 	}
 	@DeleteMapping(path = "/deleteById/{id}")
-	public ResponseEntity<?>deleteDepartment(Long id){
+	public ResponseEntity<?>deleteDepartment(@PathVariable Long id){
 		return departmentService.deleteById(id);
 	}
 	@GetMapping(path = "/getById/{id}")
-	public Department getDepartmentById(Long id) {
+	public Department getDepartmentById(@PathVariable Long id) {
 		return departmentService.getDepartmentById(id);
 	}
 }
